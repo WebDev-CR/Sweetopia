5f1b695e26e8ff84d19ddf9f9fdef56fc5340616
Aeron Glemann <aeron.glemann@gmail.com>
2011-03-13 14:38:28 -0600

    Fix for iPad

--- js/screen.js
+++ js/screen.js
@@ -1,44 +1,64 @@
 $(function(){
-	var $hud = $('.hud');
+	var touchSupported = (function(){
+	  try {
+	    return !!(document.createEvent("TouchEvent").initTouchEvent)
+	  } catch(e) {
+	    return false;
+	  };
+	})();
 	
-	$('.candy')
+	var tap = touchSupported ? 'touchstart' : 'click', 
+		$hud = $('.hud');
+	
+	var enter = function(event){
+		$('.hover').css('opacity', 0.05);
+		var $candy = $(this),
+			$hover = $candy.find('.hover').css('opacity', 0),
+			offset = $candy.offset();
+			
+		if (!$candy.data('html')){
+			$hud.find('.extras-expanded').html($candy.find('.extras').html());
+			$hud.find('.flavor').html($candy.find('input[name=flavor]').val() + ' ' + $candy.find('input[name=presentation]').val());
+			$hud.find('.price').html('¢' + $candy.find('input[name=price]').val() + '/kg');
+			$candy.data('html', $hud.html());
+		}
+		
+		offset.left += 180;
+		$hud.data('candy', this).css(offset).html($candy.data('html')).show();				
+	}
+	
+	var leave = function(event){
+		if ($hud.is(':visible')){
+			var offset = $hud.offset(), 
+				width = $hud.width(), 
+				height = $hud.height(),
+				isOver = (event.pageX > offset.left && event.pageX < offset.left + width && event.pageY > offset.top && event.pageY < offset.top + height);
+
+			if (!isOver){
+				$(this).find('.hover').css('opacity', 0.05);
+				$hud.hide();
+			}
+		}
+	}
+	
+	$candies = $('.candy')
 		.each(function(){
 			$('<div class="hover" />').appendTo(this);				
-		})
-		.hover(
-			function(event){ // mouseenter
-				$('.hover').css('opacity', 0.05);
-				var $candy = $(this),
-					$hover = $candy.find('.hover').css('opacity', 0),
-					offset = $candy.offset();
-					
-				if (!$candy.data('html')){
-					$hud.find('.extras-expanded').html($candy.find('.extras').html());
-					$hud.find('.flavor').html($candy.find('input[name=flavor]').val() + ' ' + $candy.find('input[name=presentation]').val());
-					$hud.find('.price').html('¢' + $candy.find('input[name=price]').val() + '/kg');
-					$candy.data('html', $hud.html());
-				}
-				
-				offset.left += 180;
-				$hud.css(offset).html($candy.data('html')).show();				
-			}, 
-			function(event){ // mouseleave
-				if ($hud.is(':visible')){
-					var offset = $hud.offset(), 
-						width = $hud.width(), 
-						height = $hud.height();
-					if (event.pageX > offset.left && event.pageX < offset.left + width && event.pageY > offset.top && event.pageY < offset.top + height){
-						console.log('hovering');
-					}
-					else {
-						$(this).find('.hover').css('opacity', 0.05);
-						$hud.hide();
-					}
-				}
+		});
+		
+	if (touchSupported)
+ 		$candies.bind(tap, function(event){
+			if ($hud.is(':visible') && $hud.data('candy') == this) // allow toggling for touch devices
+				leave.call(this, event);
+			else {
+				leave.call(this, event);
+				enter.call(this, event);
 			}
-		)
+		});
+	else
+		$candies.hover(enter, leave);
 	
-	$('.filter').click(function(){
+	$('.filter').bind(tap, function(){
 		var $filter = $(this),
 			classname = '.' + $filter.text().toLowerCase(),
 			fx = $filter.is('.collapsed') ? 'slideDown' : 'slideUp';
